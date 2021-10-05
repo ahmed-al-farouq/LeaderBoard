71897df1bf0090c112e67da7061f9a8564f54625
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-05 10:35:09 -0400

    Create main structure

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './main.css';
+
+const createDOM = () => {
+  const structure = `
+  <main>
+    <h1 class="title">Leader Board</h1>
+    <div class="scores">
+      <div class="header">
+        <h2>Recent Scores</h2>
+        <button>
+          Refresh
+        </button>
+      </div>
+      <ul class="body">
+        <li>name: 100</li>
+        <li>name: 20</li>
+        <li>name: 30</li>
+        <li>name: 50</li>
+        <li>name: 40</li>
+        <li>name: 60</li>
+        <li>name: 90</li>
+      </ul>
+    </div>  
+    <form action="#" method="post" class="add-score">
+      <h2>Add your score</h2>
+      <div>
+        <input type="text" name="name" id="name" placeholder="Your Name"/>
+        <input type="text" name="score" id="score" placeholder="Your Score"/>
+        <input type="submit" value="Submit"/>
+      </div>
+    </form>
+  </main>
+  `;
+  document.body.innerHTML = structure;
+  return structure;
+};
+
+createDOM();
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'output management',
+    template: './src/index.html',
+  })],
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+  },
+};

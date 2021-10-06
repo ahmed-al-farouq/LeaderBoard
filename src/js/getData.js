97482fd69aea12b0b5f56d6986693ecd3dfdb3c7
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-06 01:55:07 -0400

    Create get and post functions

---
+++ src/js/getData.js
@@ -0,0 +1,16 @@
+const getData = async () => {
+  const fetchData = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6r99l8kbAG2oZF3XXVUS/scores');
+  const result = fetchData.json();
+  const listContainer = document.getElementById('body');
+  return result.then((data) => {
+    listContainer.innerHTML = '';
+    return data.result.length
+      ? data.result.map((item) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${item.user}: ${item.score}`;
+        return listContainer.append(li);
+      })
+      : data.result;
+  });
+};
+export default getData;
